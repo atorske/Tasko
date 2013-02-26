0f54cbda17126463aff910aa1938097c1a4cd607
Mike Tolbert <mike.tolbert@gmail.com>
2013-02-26 17:36:13 -0500

    initialize in memory database with List and Card records

--- server/tasko-grails/grails-app/conf/BootStrap.groovy
+++ server/tasko-grails/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
+import task.*
 class BootStrap {
 
     def init = { servletContext ->
+      List list = new List()
+      list.name = "Test List"
+      Card c = new Card()
+      c.setDescription("Hello")
+      list.cards = []
+      list.cards << c
+      c = new Card()
+      c.setDescription("World")
+      list.cards << c
+      list.save()
     }
     def destroy = {
     }
