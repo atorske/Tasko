59d81cc4923a136f457bd1b425a2493f5aa2dc42
Mike Tolbert <mike.tolbert@gmail.com>
2013-02-26 16:16:36 -0500

    Adding RESTful endpoints for Card and List controllers.

--- server/tasko-grails/grails-app/conf/UrlMappings.groovy
+++ server/tasko-grails/grails-app/conf/UrlMappings.groovy
@@ -6,6 +6,12 @@ class UrlMappings {
 				// apply constraints here
 			}
 		}
+      "/list/$id?"(controller: "list") {
+         action = [GET:"list"]
+      }
+      "/card/$id?"(controller: "card") {
+         action = [GET:"list", POST:"save", DELETE:"delete", PUT:"edit"]
+      }
 
 		"/"(view:"/index")
 		"500"(view:'/error')
